02f415fc87b25f69123940ccf37df588894f7f6b
Marco Homeoffice <info@bossweb.eu>
2018-03-14 17:53:50 +0100

    header.php einfefügt

--- index.php
+++ index.php
@@ -1,39 +1,8 @@
 <?php
-
-
-//include('scripts/config.php')
-require_once ('scripts/session.php'); 
-
-	
+    require_once ('scripts/session.php'); 	
 ?>
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
-    <title>Dashboard FSButtler</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-grid.css" rel="stylesheet">
-    <link href="css/bootstrap-reboot.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
-  <!-- JS Script -->
-  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-
-  </head>
+<!-- Include the Header -->
+<?php include("templates/header.php"); ?>
 
   <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
---
+++ templates/header.php
@@ -0,0 +1,48 @@
+<?php
+  
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Manage your flightsimulator add-ons, manage your flights and write your own pilots logbook in one system.">
+    <meta name="author" content="Marco Boßmann">
+    
+    <!-- Social Media Meta-->
+    <meta property="og:locale" content="de_DE" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="FSBUTTLER DATABASE CENTER" />
+    <meta property="og:description" content="Manage your flightsimulator add-ons, manage your flights and write your own pilots logbook in one system." />
+    <meta property="og:url" content="https://buttler.737-homeockpit.de/" />
+    <meta property="og:site_name" content="buttler.737-Homecockpit" />
+    <meta property="og:image" content="https://737-homecockpit.de/wp-content/uploads/2016/10/logo.png" />
+    <meta property="og:image:secure_url" content="https://737-homecockpit.de/wp-content/uploads/2016/10/logo.png" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:description" content="Manage your flightsimulator add-ons, manage your flights and write your own pilots logbook in one system." />
+    <meta name="twitter:title" content="FSBUTTLER DATABASE CENTER" />
+    <meta name="twitter:image" content="https://737-homecockpit.de/wp-content/uploads/2016/10/logo.png" />
+    
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel=”apple-touch-icon” sizes=”256×256″ href=”images/apple-touch-icon-precomposed.png” />
+
+    <title>Dashboard FSButtler</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-grid.css" rel="stylesheet">
+    <link href="css/bootstrap-reboot.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+  <!-- JS Script -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+  </head>
