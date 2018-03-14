37ca4cf7ffb337d142c3fe193cdc76bab4b1ddc0
Marco Homeoffice <info@bossweb.eu>
2018-03-14 17:17:31 +0100

    config angepasst

--- scripts/config.php
+++ scripts/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'password');
+   define('DB_USERNAME', 'james');
+   define('DB_PASSWORD', 'Eqb2@8b8');
    define('DB_DATABASE', 'fsbuttler');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
