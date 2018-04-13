b217914639d21f0167fdace93ff44d327596b289
BOSSWEB <bobbybossmann@users.noreply.github.com>
2018-04-13 12:25:17 +0200

    update config.php
    new username for database

--- scripts/config.php
+++ scripts/config.php
@@ -1,8 +1,8 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
+   //define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'password');
-   //define('DB_USERNAME', 'james');
+   define('DB_USERNAME', 'fsbuttler');
    //define('DB_PASSWORD', 'Eqb2@8b8');
    define('DB_DATABASE', 'fsbuttler');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
